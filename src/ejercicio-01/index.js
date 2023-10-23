a6b275eeba9c201bc41eccda563a1962f8352566
forersa <forersa@yahoo.com>
2023-10-23 20:43:10 +0200

    AñadirEjercicio001

---
+++ src/ejercicio-01/index.js
@@ -0,0 +1,13 @@
+const nombre = 'Sander'
+const edad = 56
+const esEstudiante = true
+const mensaje = "Hola, mi nombre es"+nombre+"y tengo"+edad+ "años.   Soy estudiante: "+ esEstudiante+ "."
+
+const snake_case = "snake_case"
+const camelCase = "camelCase"
+
+const PascalCase = "PascalCase"
+const kebab_case = "kebab-case"
+//. para colocar comentarios.
+
+console.log('Mensaje:', mensaje)
